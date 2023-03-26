cba77e1ae56cf8d83cae8482c29b0ab3ed0f1945
GabrielFdeOliveira <gbl.diesel@hotmail.com>
2023-03-26 14:23:43 +0100

    Title tag added to Head component at document file

--- Frontend/widgets/src/pages/_document.js
+++ Frontend/widgets/src/pages/_document.js
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Wally's Widgets</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
